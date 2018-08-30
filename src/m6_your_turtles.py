64d9c0f0e73bbb2f96f5db75a80d42563195aa9f
bowmanj1 <bowmanj1@rose-hulman.edu>
2018-08-30 18:11:56 -0400

    j

--- src/m6_your_turtles.py
+++ src/m6_your_turtles.py
@@ -31,4 +31,25 @@ import rosegraphics as rg
 
 window = rg.TurtleWindow
 
+size1 = 200
 turtle1 = rg.SimpleTurtle('turtle')
+turtle1.pen = rg.Pen('green', 2)
+turtle1.speed = 10
+
+for k in range(18):
+    turtle1.draw_circle(size1)
+    turtle1.pen_up()
+    turtle1.backward(10)
+    turtle1.pen_down()
+    size1 = size1 - 10
+
+turtle2 = rg.SimpleTurtle()
+turtle2.pen = rg.Pen('red', 12)
+turtle2.speed = 3
+turtle2.right(72)
+
+for k in range(5):
+    turtle2.forward(100)
+    turtle2.right(144)
+
+window.close_on_mouse_click()
