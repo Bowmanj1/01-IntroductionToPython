21b4484b7237f5075c8ecf02706a9337de07293e
bowmanj1 <bowmanj1@rose-hulman.edu>
2018-08-30 15:54:24 -0400

    j

--- src/m2_todo_and_commit_push.py
+++ src/m2_todo_and_commit_push.py
@@ -1,5 +1,5 @@
 print('Hello, World')
-
+print('My name is Jacob')
 ###############################################################################
 #
 #   This line is a COMMENT -- a note to human readers of this file.
@@ -17,7 +17,7 @@ print('Hello, World')
 
 ###############################################################################
 #
-# TODO: 1.
+# DONE: 1.
 #   (Yes, that means for YOU to DO things per these instructions:)
 #
 #   Run this module by right clicking anywhere in this window and selecting
@@ -32,7 +32,7 @@ print('Hello, World')
 
 ###############################################################################
 #
-# TODO: 2.
+# Done: 2.
 #   Notice the small horizontal BLUE bars on the scrollbar-like thing
 #   on the right.  Each blue bar indicates a thing TO-DO in this module.
 #
@@ -57,7 +57,7 @@ print('Hello, World')
 
 ###############################################################################
 #
-# TODO: 3.
+# DONE: 3.
 #   Add another   print   statement below the current Line 1 above.
 #   It should print any string that you want (but keep it G-rated!)
 #
