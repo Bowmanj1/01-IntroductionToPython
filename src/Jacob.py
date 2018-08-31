e5597ee15ad6042e54752b544f1c1d695f592715
bowmanj1 <bowmanj1@rose-hulman.edu>
2018-08-31 11:03:59 -0400

    j

--- src/Jacob.py
+++ src/Jacob.py
@@ -1,13 +1,20 @@
 import rosegraphics as rg
 window = rg.TurtleWindow
 turtle2 = rg.SimpleTurtle()
-turtle2.pen = rg.Pen('red', 12)
+turtle2.pen = rg.Pen('red', 4)
 turtle2.speed = 30
-turtle2.right(72)
-for k in range(30):
-    for k in range(5):
-        turtle2.forward(200)
-        turtle2.right(144)
-    turtle2.right(18)
-    turtle2.forward(18)
+for k in range(10):
+    for k in range(15):
+        for k in range(3):
+            turtle2.draw_circle(k ** k)
+            turtle2.forward(10 * k)
+        turtle2.left((180/15) * k)
+    turtle2.pen_up()
+    turtle2.forward((10 - k) ** 2)
+    turtle2.pen_down()
 window.close_on_mouse_click()
+for k in range(5):
+    for k in range(15):
+        turtle2.draw_circle(100)
+        turtle2.left(24)
+    turtle2.forward(k)
